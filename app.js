82a6ef86a887d5d7afe7af19b7c7d0fab431bed3
Ravindra Warnasooriya <warnasooriya.ravindra@gmail.com>
2025-03-22 19:28:00 +0530

    applied dockker

--- app.js
+++ app.js
@@ -28,6 +28,6 @@ app.use("/api", availibilityRoutes);
 app.use("/api", specializationRouter);
 
 // Start server
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
